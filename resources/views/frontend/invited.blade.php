ab1fa3e8c2f39bdd904b2815ef94d31bf84d4ba3
cagriel <cagriel08@gmail.com>
2022-08-09 15:31:20 +0300

    fix

--- resources/views/frontend/invited.blade.php
+++ resources/views/frontend/invited.blade.php
@@ -21,53 +21,16 @@ use App\Models\News;
   <body>
     <div class="container">
       <div class="row">
-      <div class="col-md-6">
         @foreach (News::all() as $news)
-        <div class="row" style="padding-bottom:30px;">
-          <div class="col-md-12">
+          <div class="col-md-6">
             <span class="number-box" style="float:left;margin-right:24px;">{{$news->intived_number}}</span>
             <p class="invited">
-              
               {!!html_entity_decode($news->invited_talks)!!}
             </p>
           </div>
-        </div>
         @endforeach
       </div>
-      {{-- <div class="col-md-6">
-        <div class="row" style="padding-bottom:30px;">
-          <div class="col-md-12">
-            <span class="number-box" style="float:left;margin-right:24px;">2</span>
-            <p class="invited">Transautophagy Meeting. 6-7 October, 2016, Warsaw, Poland.</p>
-          </div>
-        </div>
-        <div class="row" style="padding-bottom:30px;">
-          <div class="col-md-12">
-            <span class="number-box" style="float:left;margin-right:24px;">4</span>
-            <p class="invited">EMBO (European Molecular Biology Organization)-Cancer Sectoral Meeting. 2-4 September 2015, Prague, Czech Republic.</p>
-          </div>
-        </div>
-        <div class="row" style="padding-bottom:30px;">
-          <div class="col-md-12">
-            <span class="number-box" style="float:left;margin-right:24px;">6</span>
-            <p class="invited">    International Cell Death Society (ICDS) Conference on: “Cell Death and its Translational Ramification, 2-5 June, 2016, Cork, Ireland.</p>
-          </div>
-        </div>
-        <div class="row" style="padding-bottom:30px;">
-          <div class="col-md-12">
-            <span class="number-box" style="float:left;margin-right:24px;">8</span>
-            <p class="invited"> Healthy Minor Cereals Meeting. 10-11 May 2016, Berlin, Germany.</p>
-          </div>
-        </div>
-        <div class="row" style="padding-bottom:30px;">
-          <div class="col-md-12">
-            <span class="number-box" style="float:left;margin-right:24px;">10</span>
-            <p class="invited">Black Forest Winter Conference on Autophagic Membrane Trafficking and Dynamics in Ageing and Disease, 29-31 January 2016, Freiburg, Germany.</p>
-          </div>
-        </div>
-      </div> --}}
     </div>
-  </div>
   <div class="container">
     <div class="row">
       <div class="col-md-12">
