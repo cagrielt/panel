58e38314ada19133e79c14a8b3ac46a256402983
cagriel <cagriel08@gmail.com>
2022-08-10 16:24:17 +0300

    fix

--- app/Http/Requests/Backend/Publications/ManagePublicationsRequest.php
+++ app/Http/Requests/Backend/Publications/ManagePublicationsRequest.php
@@ -17,7 +17,7 @@ class ManagePublicationsRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
+     * Get the validation rules that apply to the request.  
      *
      * @return array
      */
